1c0e74f2b95664d0135cd0ce537767eadf2a065b
christyorji <christyorji@hotmail.co.uk>
2023-07-13 16:29:59 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,12 +11,25 @@ int main(void)
 {
 	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	ssize_t len = strlen(message);
+	ssize_t bytes_written = 0;
+	int write_error = 0;
 
-	if (write(STDERR_FILENO, message, len) != len)
+	while (bytes_written < len)
+	{
+		ssize_t result = write(STDERR_FILENO, message + bytes_written, len - bytes_written);
+		if (result < 0)
+		{
+			write_error = 1;
+			break;
+		}
+		bytes_written += result;
+	}
+
+	if (write_error)
 	{
 		/* Error writing to stderr */
 		_exit(1);
 	}
 
-	return (1);
+	return 1;
 }
