4ea49cd99e38dff7e82f191152eb5e93f36be027
christyorji <christyorji@hotmail.co.uk>
2023-07-13 17:50:29 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,12 @@
-#include <unistd.h>
-#include <string.h>
-#include <errno.h>
-
+#include<stdio.h>
 /**
- * main - Entry point of the program
- *
- * Return: Always 1 (indicating program execution)
- */
+* main - entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-        fprintf(stdout, "and that piece of art is useful\" - ");
-	fprintf(stdout, "Dora Korpar, 2015-10-19\n");
-        ssize_t len = strlen(message);
-
-	if (write(STDERR_FILENO, message, len) != len)
-	{
-		/* Error writing to stderr */
-		_exit(1);
-	}
-
-	return (1);
+fprintf(stdout, "and that piece of art is useful\" - ");
+fprintf(stdout, "Dora Korpar, 2015-10-19\n");
+return (1);
 }
