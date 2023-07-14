a03dba017d7ec1fc0e51488d3ba15d0d5994dcc9
christyorji <christyorji@hotmail.co.uk>
2023-07-13 17:39:42 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,9 @@
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t len = strlen(message);
+        fprintf(stdout, "and that piece of art is useful\" - ");
+	fprintf(stdout, "Dora Korpar, 2015-10-19\n");
+        ssize_t len = strlen(message);
 
 	if (write(STDERR_FILENO, message, len) != len)
 	{
