0db2eca23525f350eb170412ec60a7c57402b179
christyorji <christyorji@hotmail.co.uk>
2023-07-18 14:52:58 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,11 @@
 #include "main.h"
+
 /**
  * main - Entry point
+ *
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 _putchar('_');
@@ -12,7 +15,7 @@ _putchar('t');
 _putchar('c');
 _putchar('h');
 _putchar('a');
-_putchar('r'); 
+_putchar('r');
 _putchar('\n');
 return (0);
 }
