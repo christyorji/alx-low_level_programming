1bf7dcf2b8bfd8cc2f94894d2711363b6a3ff42f
christyorji <christyorji@hotmail.co.uk>
2023-07-18 13:07:30 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* main – Entry point of my code,
+*
+* Return: Always 0
+*/
+int _main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
+
