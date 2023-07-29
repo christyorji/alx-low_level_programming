e5fed0366c3a8ae0ec216a7229a23579487270c5
christyorji <christyorji@hotmail.co.uk>
2023-07-28 19:28:36 -0700

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,21 +8,21 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-		int lengthD, lengthS;
+int lengthD, lengthS;
 
-			lengthD = 0;
-				lengthS = 0;
+lengthD = 0;
+lengthS = 0;
 
-					while (*(dest + lengthD) != '\0')
-								lengthD++;
+while (*(dest + lengthD) != '\0')
+lengthD++;
 
-						while (*(src + lengthS) != '\0' && lengthD < 97 && lengthS < n)
-								{
-											*(dest + lengthD) = *(src + lengthS);
-													lengthD++;
-															lengthS++;
+while (*(src + lengthS) != '\0' && lengthD < 97 && lengthS < n)
+{
+*(dest + lengthD) = *(src + lengthS);
+lengthD++;
+lengthS++;
 																}
-							*(dest + lengthD) = '\0';
-								return (dest);
+*(dest + lengthD) = '\0';
+return (dest);
 }
 
