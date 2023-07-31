2990548aade0bdf38f21e77431ca5039605f2baa
christyorji <christyorji@hotmail.co.uk>
2023-07-31 15:19:47 -0700

    6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_chessboard - print a chessboard.
+ * @a: source matrix
+ *
+ *
+ * Return: chessboard.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for(j = 0; j <= 7; j++)
+		for (j = 0; j <= 7; j++)
+		{
+			putchar(a[i][j]);
+		}
+		putchar('\n');
+	}
+}
