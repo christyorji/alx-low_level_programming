6ddcf33ede728b925e5f00d7c420d557838d4829
christyorji <christyorji@hotmail.co.uk>
2023-07-31 15:26:20 -0700

    6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,11 +12,11 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i <= 7; i++)
 	{
-		for(j = 0; j <= 7; j++)
+		for (j = 0; j <= 7; j++)
 		for (j = 0; j <= 7; j++)
 		{
-			putchar(a[i][j]);
+			putchar (a[i][j]);
 		}
-		putchar('\n');
+		putchar ('\n');
 	}
 }
