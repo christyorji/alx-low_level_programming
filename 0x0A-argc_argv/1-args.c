16b47271eb3859db4617a5361bdb961a47f52bf6
christyorji <christyorji@hotmail.co.uk>
2023-08-04 12:29:22 -0700

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,14 +3,13 @@
 
 /**
  * main - prints the number of arguments passed into it
- *
- * @argc: Counts the number of command line argument 
+ * @argc:counts the number of command line argument
  * @argv: Pointer of array of pointers containing strings entering main
  * Return: Always 0 (Successful)
  */
 int main(int argc, char **argv)
 {
-	(void) argv;
-	printf("%i\n", argc - 1);
-	return (0);
+(void) argv;
+printf("%i\n", argc - 1);
+return (0);
 }
