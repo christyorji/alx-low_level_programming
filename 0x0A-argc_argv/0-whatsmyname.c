3a95c313a0a50a9f7455fa406303b298b157055e
christyorji <christyorji@hotmail.co.uk>
2023-08-04 11:22:33 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main-prints its name, followed by a new line
+ *
+ * @argc: counts the number of command line arguments
+ * @argv: array of pointers that contain the command line argv
+ *
+ * Return :(0) Always sucessfull
+ */
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+printf("%s\n", argv[0]);
+return (0);
+}
